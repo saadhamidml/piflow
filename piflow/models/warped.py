Manage scaling of integrands. Fix hyperparameter issues for BQ and MMLT. They persist for WSABI though

piflow/models/warped.py
@@ -3,7 +3,10 @@ from gpflow.base import InputData, MeanAndVariance, RegressionData
 from gpflow.models import GPR
 from gpflow.posteriors import GPRPosterior
 import tensorflow as tf
+from trieste.data import Dataset
 from trieste.models.gpflow.models import GaussianProcessRegression
+
+from .transforms import DataTransformMixin
 
 
 class WarpedGPR(GPR):
@@ -75,7 +78,7 @@ class MMLT_GPR(WarpedGPR):
     """Moment matched log transform model."""
 
     def _warp(self, y: tf.Tensor) -> tf.Tensor:
-        return tf.math.log(y) 
+        return tf.math.log(y + 1e-9) 
 
     def predict_f(
         self,
@@ -99,3 +102,16 @@ class MMLT_GPR(WarpedGPR):
         else:
             f_cov = tf.math.expm1(g_var_diag) * f_mean ** 2 
         return f_mean, f_cov 
+    
+
+class _WarpedGaussianProcessRegression(GaussianProcessRegression):
+    """Trieste Model for Warped GPR."""
+
+    def update(self, dataset: Dataset) -> None:
+        self.model.unwarped_Y_data = dataset.observations
+        warped_dataset = Dataset(dataset.query_points, self.model._warp(dataset.observations))
+        return super().update(warped_dataset)
+
+
+class WarpedGaussianProcessRegression(DataTransformMixin, _WarpedGaussianProcessRegression):
+    pass

piflow/probabilistic_integrator.py
@@ -7,7 +7,11 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import trieste
 
-from .models import WSABI_L_GPR, MMLT_GPR
+from piflow.models.transforms import (
+    DataTransformMixin, IdentityTransformer, StandardTransformer, MinMaxTransformer
+)
+
+from .models.warped import WSABI_L_GPR, MMLT_GPR
 from gpmaniflow.models import LogBezierProcess
 
 NoneType = type(None)
@@ -62,6 +66,19 @@ class IntegrandModel():
             self._integral_variance = int_variance
         else:
             int_variance = self._integral_variance
+        if isinstance(self._model, DataTransformMixin):
+            if not isinstance(self._model._query_point_transformer, IdentityTransformer):
+                raise NotImplementedError
+            if (
+                not isinstance(self._model._observation_transformer, StandardTransformer)
+                and not isinstance(self._model._observation_transformer, MinMaxTransformer)
+            ):
+                raise NotImplementedError
+            # Assumes the prior integrates to 1.
+            int_mean = self._model._observation_transformer.inverse_transform(int_mean)
+            int_variance = self._model._observation_transformer.inverse_transform_variance(
+                int_variance
+            )
         return int_mean, int_variance
 
 
@@ -451,8 +468,8 @@ def _mmlt_var(
     num_samples = 1000 * X.shape[1]
     samples1 = prior.sample(num_samples)
     samples2 = prior.sample(num_samples)
-    f_mean1, _ = model.predict_g(samples1, posterior=posterior)
-    f_mean2, _ = model.predict_g(samples2, posterior=posterior)
+    f_mean1, _ = model.predict_f(samples1, posterior=posterior)
+    f_mean2, _ = model.predict_f(samples2, posterior=posterior)
     covar_factors = f_mean1 * f_mean2
     cross_cov = tf.linalg.diag_part(kernel.K(samples1, samples2)) - tf.linalg.diag_part(
         kernel.K(samples1, X) @ posterior.cache[0] @ kernel.K(X, samples2)

tests/test_integrand_model.py
@@ -1,16 +1,19 @@
 import gpflow
+import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 from trieste.models.gpflow.models import GaussianProcessRegression
 import pytest
+from piflow.models.transforms import DataTransformMixin, MinMaxTransformer, StandardTransformer
+from piflow.models.utils import initialise_lengthscales, set_model_priors
 
-from piflow.models import MMLT_GPR, WSABI_L_GPR
+from piflow.models.warped import MMLT_GPR, WSABI_L_GPR, WarpedGaussianProcessRegression
 from piflow.probabilistic_integrator import IntegrandModel
 
 
-# @pytest.mark.parametrize('warping', [None, 'WSABI-L', 'MMLT'])
-# @pytest.mark.parametrize('prior_type', ['gaussian', 'uniform'])
-def test_integrand_model(warping: str = None, prior_type: str = 'gaussian'):
+@pytest.mark.parametrize('warping', [None, 'WSABI-L', 'MMLT'])
+@pytest.mark.parametrize('prior_type', ['gaussian', 'uniform'])
+def test_integrand_model(warping: str, prior_type: str):
     # Generate data from a Gaussian.
     integrand = tfp.distributions.MultivariateNormalFullCovariance(
         loc=tf.constant([1., 1.], dtype=tf.float64),
@@ -37,33 +40,50 @@ def test_integrand_model(warping: str = None, prior_type: str = 'gaussian'):
         true_integral = tf.reduce_prod(cdf(prior.high) - cdf(prior.low))
     X = integrand.sample(500)
     Y = tf.reshape(integrand.prob(X), (-1, 1))
+    lengthscales = initialise_lengthscales(X)
+    kernel = gpflow.kernels.SquaredExponential(lengthscales=lengthscales)
     # Set up and train integrand model
     if warping is None:
-        gpflow_model = gpflow.models.GPR((X, Y), gpflow.kernels.SquaredExponential())
+        observation_transformer = StandardTransformer(Y)
+        Y_ = observation_transformer.transform(Y)
+        gpflow_model = gpflow.models.GPR((X, Y_), kernel)
+        class PIFlowModel(DataTransformMixin, GaussianProcessRegression):
+            pass
     elif warping == 'WSABI-L':
         if prior_type == 'uniform':
             old_prior = prior
             prior = tfp.distributions.MultivariateNormalFullCovariance(
                 loc=tf.constant([0.5, 0.5], dtype=tf.float64),
-                covariance_matrix=tf.constant([[0.33, 0.], [0., 0.33]], dtype=tf.float64)
+                covariance_matrix=tf.constant([[1., 0.], [0., 1.]], dtype=tf.float64)
             )
             Y = Y * tf.reshape(tf.reduce_prod(old_prior.prob(X), axis=1), (-1, 1)) / tf.reshape(prior.prob(X), (-1, 1))
-        gpflow_model = WSABI_L_GPR((X, Y), gpflow.kernels.SquaredExponential())
+        observation_transformer = MinMaxTransformer(Y)
+        Y_ = observation_transformer.transform(Y)
+        gpflow_model = WSABI_L_GPR((X, Y_), kernel)
+        # TODO: This is essentially heuristic; initialise by sampling instead.
+        gpflow.set_trainable(gpflow_model.kernel.variance, False)
+        gpflow.set_trainable(gpflow_model.kernel.lengthscales, False)
+        PIFlowModel = WarpedGaussianProcessRegression
     elif warping == 'MMLT':
-        gpflow_model = MMLT_GPR((X, Y), gpflow.kernels.SquaredExponential())
-    model = GaussianProcessRegression(gpflow_model)
+        observation_transformer = MinMaxTransformer(Y)
+        Y_ = observation_transformer.transform(Y)
+        gpflow_model = MMLT_GPR((X, Y_), kernel)
+        PIFlowModel = WarpedGaussianProcessRegression
+    set_model_priors(gpflow_model)
+    model = PIFlowModel(gpflow_model, observation_transformer=observation_transformer)
     integrand_model = IntegrandModel(prior, model)
     opt = gpflow.optimizers.Scipy()
-    # opt.minimize(gpflow_model.training_loss, gpflow_model.trainable_variables)
+    opt.minimize(gpflow_model.training_loss, gpflow_model.trainable_variables)
+    gpflow.utilities.print_summary(gpflow_model)
     # Compute integral.
     int_mean, int_var = integrand_model.integral_posterior()
     # Assert approximation is close to true value.
-    tf.debugging.assert_near(true_integral, int_mean)
+    tf.debugging.assert_near(true_integral, int_mean, rtol=0.1)
     
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # test_integrand_model(warping=None, prior_type='gaussian')
-    test_integrand_model(warping=None, prior_type='uniform')
+    # test_integrand_model(warping=None, prior_type='uniform')
     # test_integrand_model(warping='WSABI-L', prior_type='gaussian')
     # test_integrand_model(warping='WSABI-L', prior_type='uniform')
     # test_integrand_model(warping='MMLT', prior_type='gaussian')